286742f76b1dd31a80a4eb3dc89118f47364d255
aocattleya <aocattleya@yahoo.co.jp>
2019-10-25 17:23:05 +0900

    add: 投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,14 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    Post.create(post_params)
+    redirect_to root_path
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
