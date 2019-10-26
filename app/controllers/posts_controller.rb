f17d775f13e65d914b0f983b7fb6a265b6813a36
aocattleya <aocattleya@yahoo.co.jp>
2019-10-27 00:58:30 +0900

    add: ブログ削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,6 +26,12 @@ class PostsController < ApplicationController
     redirect_to post_path(post.id)
   end
 
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :content)
