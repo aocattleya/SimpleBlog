9ee67bde33140b073cd467cb4707d58b7d124518
aocattleya <aocattleya@yahoo.co.jp>
2019-10-27 00:44:51 +0900

    add: ブログ編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :content)
